bc95421d84f24851aaca078a8f7c7d3d659b5833
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-06 12:41:45 +0100

    ruff check

--- src/cluster_api/util/client_setup.py
+++ src/cluster_api/util/client_setup.py
@@ -1,7 +1,15 @@
 from kubernetes import client, config
 
 
+
 def get_api_client():
+<<<<<<< HEAD:src/cluster_api/util/client_setup.py
     """Return a CoreV1Api client from the auth folder."""
     config.load_kube_config()
+=======
+    """Return the client API based of the k3s.yaml.
+
+    which should be manually put into the auth folder.
+    """
+>>>>>>> 2ac5e97 (ruff check):kubernetes/util/client_setup.py
     return client.CoreV1Api()
