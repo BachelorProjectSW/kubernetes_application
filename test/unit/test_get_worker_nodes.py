3d51378881ce7f98c568d4955290c887db01a535
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-09 15:05:35 +0100

    code actually works now

--- test/unit/test_get_worker_nodes.py
+++ test/unit/test_get_worker_nodes.py
@@ -3,8 +3,6 @@ from cluster_api.services.get_worker_nodes import get_worker_nodes
 
 
 @pytest.mark.unit
-def test_get_worker_nodes(mocker):
+def test_get_worker_nodes():
     """Test on historical data and not the API call"""
-    with pytest.raises(Exception):
-        get_worker_nodes
-    assert 1
+    assert [{'name':'pantrum2'}] == get_worker_nodes()
