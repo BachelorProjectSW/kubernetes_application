3fd55ceefaf2909886458c81b849ad56dde47f44
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-11 14:43:16 +0100

    more true doc

--- src/cluster_api/util/client_setup.py
+++ src/cluster_api/util/client_setup.py
@@ -6,8 +6,5 @@ config.load_kube_config(KUBECONFIG)
 
 
 def get_api_client():
-    """Return the client API based of the k3s.yaml.
-
-    which should be manually put into the auth folder.
-    """
+    """Return the client, which is inside the /auth folder."""
     return client.CoreV1Api()
