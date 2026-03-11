36ec23f7688d5cc83511fa9956396322db0621f4
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-11 11:45:13 +0100

    maybe working

--- test/k3d/deploy.sh
+++ test/k3d/deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Use the kubeconfig for the k3d cluster
-export KUBECONFIG=$(k3d kubeconfig get devcluster)
-
-# Wait until all nodes are ready
 kubectl wait --for=condition=Ready nodes --all --timeout=120s
 
-# Apply manifests
 kubectl apply -f src/cluster_api/manifest/
---
+++ test/k3d/setup_kubernetes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+#Download k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+#Get CPU type
+ARCH=$(uname -m)
+
+case "$ARCH" in
+    x86_64)
+        C_ARCH="amd64"
+        ;;
+    aarch64 | arm64)
+        C_ARCH="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+
+#install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/$C_ARCH/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+
