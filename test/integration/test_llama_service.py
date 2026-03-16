2ec097e03fc8a8b17f39fe6bce6bde0939be362c
omid <omid745433@gmail.com>
2026-03-16 17:00:00 +0100

    bruh

--- test/integration/test_llama_service.py
+++ test/integration/test_llama_service.py
@@ -2,5 +2,7 @@ import requests
 
 
 def test_llama_service_models_endpoint():
+    """Test llama endpoint."""
     response = requests.get("http://127.0.0.1:8080/v1/models", timeout=15)
+
     assert response.status_code == 200
