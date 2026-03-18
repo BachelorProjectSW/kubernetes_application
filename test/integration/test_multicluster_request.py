4aa72cd69fc1aa8856149a2c36ba9bb673317274
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-18 10:45:17 +0100

    multicluster init

--- test/integration/test_multicluster_request.py
+++ test/integration/test_multicluster_request.py
@@ -9,5 +9,4 @@ def test_multi_cluster_request():
     dk_response = requests.get(f"{DK_CLUSTER_URL}/v1/models", timeout=15)
     po_response = requests.get(f"{PO_CLUSTER_URL}/v1/models", timeout=15)
     assert dk_response.status_code == 200
-    assert po_repsonse.status_code == 200
-
+    assert po_response.status_code == 200
