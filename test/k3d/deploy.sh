9df7a34d0e92d1864234f62ff072202db6970838
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-18 10:22:47 +0100

    its working

--- test/k3d/deploy.sh
+++ test/k3d/deploy.sh
@@ -7,10 +7,11 @@ source test/k3d/cluster_name_list.sh
 
 for cluster_name in ${CLUSTER_NAMES[@]}; do
 	export KUBECONFIG="src/cluster_api/auth/k3d-devcluster-${cluster_name}.yaml"
-
+	echo "waiting for nodes"
 	kubectl wait --for=condition=Ready nodes --all --timeout=120s
-
+	echo "nodes are ready"
 	kubectl apply -f src/cluster_api/manifest/
-
+	echo "applying ${cluster_name} manifest"
 	kubectl wait --for=condition=Ready pod -l name=llama-server --timeout=180s
+	echo "all pods ready"
 done
