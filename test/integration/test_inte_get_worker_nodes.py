fdbb1b3194f1c859985871c95b8850a484513273
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-18 11:57:40 +0100

    multicluster init

--- test/integration/test_inte_get_worker_nodes.py
+++ test/integration/test_inte_get_worker_nodes.py
@@ -5,8 +5,7 @@ from cluster_api.services.get_worker_nodes import get_all_nodes
 @pytest.mark.integration
 def test_get_worker_nodes():
     """Test its working while the kubernetes is deployed."""
-    print(get_all_nodes())
-    assert [
-        {'name': 'k3d-devcluster-dk-agent-0'},
-        {'name': 'k3d-devcluster-po-agent-0'}
+    assert
+        {'name': 'k3d-devcluster-po-agent-0'},
+        {'name': 'k3d-devcluster-dk-agent-0'}
     ] == get_all_nodes()
