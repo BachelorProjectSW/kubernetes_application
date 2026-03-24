e47a437cd7fe093f8c7a7c32d18cb64139481004
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-03-24 17:35:40 +0100

    tg

--- src/cluster_api/services/handle_question_request.py
+++ src/cluster_api/services/handle_question_request.py
@@ -1,12 +1,4 @@
-import requests
-from ..util.cluster_connection import get_all_clusters_endpoint
-
 def handle_question_request(question: str):
     """Handle question reqeust temp."""
-    for cluster, endpoint in get_all_clusters_endpoint().items():
-        url = f"http://{endpoint}/llm"
-        response = requests.get(url, timeout=5)
-        worker_nodes.extend(response.json())
-
-    return worker_nodes
+    print(question)
 
