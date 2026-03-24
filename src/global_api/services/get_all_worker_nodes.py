3a8fb841f67187c4be9a8f9be200090303b3ef30
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-03-24 17:31:45 +0100

    whoops

--- src/global_api/services/get_all_worker_nodes.py
+++ src/global_api/services/get_all_worker_nodes.py
@@ -1,11 +1,11 @@
 import requests
-from ..util.cluster_connection import get_all_clusters_endpoint
+from ..util.cluster_connection import get_all_clusters_config
 
 
 def get_all_worker_nodes():
     """Return all working nodes for each cluster."""
     worker_nodes = []
-    for cluster, config in get_all_clusters_endpoint().items():
+    for cluster, config in get_all_clusters_config().items():
         url = f"http://{config["ip"]:{config["port"]}}/get_cluster_nodes"
         response = requests.get(url, timeout=5)
         worker_nodes.extend(response.json())
