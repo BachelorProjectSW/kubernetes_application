ef20c92b01dbed0907f4401ad0ccf7a0a8967f6d
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-03-25 09:28:54 +0100

    handling tests

--- test/integration/test_multicluster_request.py
+++ test/integration/test_multicluster_request.py
@@ -1,12 +1,7 @@
-import requests
-
-DK_CLUSTER_URL = "http://127.0.0.1:8888"
-PO_CLUSTER_URL = "http://127.0.0.1:8889"
+from global_api.services.handle_llm_request import handle_llm_request
 
 
 def test_multi_cluster_request():
     """Test endpoint from two clusters."""
-    dk_response = requests.get(f"{DK_CLUSTER_URL}/v1/models", timeout=15)
-    po_response = requests.get(f"{PO_CLUSTER_URL}/v1/models", timeout=15)
-    assert dk_response.status_code == 200
-    assert po_response.status_code == 200
+    response = handle_llm_request("What is 2+2")
+    assert response.status_code == 200
