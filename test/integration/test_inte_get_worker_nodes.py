4c29e5e0e673881814235e7f802b1f3708f8fb3d
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-26 12:31:51 +0100

    parent

--- test/integration/test_inte_get_worker_nodes.py
+++ test/integration/test_inte_get_worker_nodes.py
@@ -6,7 +6,7 @@ import global_api.util.cluster_connection as cluster_connection
 @pytest.mark.integration
 def test_get_worker_nodes(monkeypatch):
     """Test to get all worker nodes using different cluster config such as ip addresses."""
-    test_config_path = Path(__file__).parent / "k3d" / "cluster_configs" / "test_clusters.yaml"
+    test_config_path = Path(__file__).parents[1] / "k3d" / "cluster_configs" / "test_clusters.yaml"
 
     # Patch CONFIG_PATH to the test_config
     monkeypatch.setattr(cluster_connection, "CONFIG_PATH", test_config_path)
