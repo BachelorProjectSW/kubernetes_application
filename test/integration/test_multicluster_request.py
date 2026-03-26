feeaed46f859a4455919c112c17be61b6b6127e9
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-26 12:35:24 +0100

    ip fixed!

--- test/integration/test_multicluster_request.py
+++ test/integration/test_multicluster_request.py
@@ -1,9 +1,11 @@
+from pathlib import Path
+import pytest
 import global_api.util.cluster_connection as cluster_connection
 
 
-def test_multi_cluster_request():
+def test_multi_cluster_request(monkeypatch):
     """Test endpoint from multiple clusters."""
-    test_config_path = Path(__file__).parent / "k3d" / "cluster_configs" / "test_clusters.yaml"
+    test_config_path = Path(__file__).parents[1] / "k3d" / "cluster_configs" / "test_clusters.yaml"
 
     # Patch CONFIG_PATH to the test_config
     monkeypatch.setattr(cluster_connection, "CONFIG_PATH", test_config_path)
@@ -11,10 +13,9 @@ def test_multi_cluster_request():
     # Now after patch get the function as CONFIG_PATH is constant and read when imported.
     from global_api.services.handle_llm_request import handle_llm_request
 
-    for _ in range(5):
-        response = handle_llm_request("Describe kubernetes")
+    response = handle_llm_request("Describe kubernetes")
 
-        content = response['choices'][0]['message']['content']
+    content = response['choices'][0]['message']['content']
 
-        assert response
-        assert content
+    assert response
+    assert content
