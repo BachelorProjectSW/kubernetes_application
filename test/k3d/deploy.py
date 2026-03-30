bfc4b9aaf30d6e0a849de8c05fe23488f2fd6aba
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-30 15:04:54 +0200

    gg

--- test/k3d/deploy.py
+++ test/k3d/deploy.py
@@ -13,9 +13,13 @@ def deploy_clusters():
 
         run_cmd("kubectl wait --for=condition=Ready nodes --all --timeout=120s")
         run_cmd("kubectl apply -f src/cluster_api/manifest/")
-        for _ in range(10):
+        for _ in range(3):
             time.sleep(10)
             run_cmd("kubectl get pods -o wide")
+        run_cmd(
+           "kubectl --kubeconfig src/cluster_api/auth/k3d-devcluster-dk.yaml 
+            logs -l name=llama-server --all-containers"
+        )
         run_cmd("kubectl wait --for=condition=Ready pod -l name=llama-server --timeout=180s")
 
 
