6df0cd390f121570d6aeb05caa30ac5999c2898c
DoubleO69420 <omid745433@gmail.com>
2026-03-31 13:32:24 +0200

    ere

--- src/cluster_api/services/get_worker_nodes.py
+++ src/cluster_api/services/get_worker_nodes.py
@@ -6,23 +6,26 @@ log = structlog.get_logger()
 
 def get_cluster_working_nodes():
     """Return a JSON object with all the active working nodes."""
-    api_client = get_api_client()
-    nodes = api_client.list_node()
-    worker_nodes = []
-    for node in nodes.items:
-        # Skip control plane
-        labels = str(node.metadata.labels) or ""  # Empty if not exist instead of crash
-        if "'node-role.kubernetes.io/control-plane': 'true'" in labels:
-            log.debug("node.skipped", name=node.metadata.name, reason="control-plane")
-            continue
+    try:
+        api_client = get_api_client()
+        nodes = api_client.list_node()
+        worker_nodes = []
+        for node in nodes.items:
+            # Skip control plane
+            labels = str(node.metadata.labels) or ""  # Empty if not exist instead of crash
+            if "'node-role.kubernetes.io/control-plane': 'true'" in labels:
+                log.debug("node.skipped", name=node.metadata.name, reason="control-plane")
+                continue
 
-        name = node.metadata.name
+            name = node.metadata.name
 
-        worker_nodes.append({
-            "name": name
-            })
+            worker_nodes.append({
+                "name": name
+                })
 
-    node_names = [n["name"] for n in worker_nodes]
-    log.info("worker_nodes.found", count=len(worker_nodes), nodes=node_names)
+        node_names = [n["name"] for n in worker_nodes]
+        log.info("worker_nodes.found", count=len(worker_nodes), nodes=node_names)
 
-    return worker_nodes
+        return worker_nodes
+    except Exception as e:
+        return e
