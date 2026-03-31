4f35c0c1bdfae26e3881c41c9c9b94fdddc6e464
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-31 16:27:12 +0200

    gg

---
+++ src/models/nodes.py
@@ -0,0 +1,11 @@
+class WorkerNode:
+    """All information of a worker node"""
+    def __init__(self):
+        self.name = ""
+        self.ip = ""
+        self.status = ""
+        self.gpio = ""
+        self.logs = ""
+
+    def assign_gpio():
+        
