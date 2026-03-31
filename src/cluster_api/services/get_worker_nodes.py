d30d3c1cc916a2215820c3d63682aeaa6857474f
DoubleO69420 <omid745433@gmail.com>
2026-03-31 13:37:52 +0200

    erre

--- src/cluster_api/services/get_worker_nodes.py
+++ src/cluster_api/services/get_worker_nodes.py
@@ -9,6 +9,7 @@ def get_cluster_working_nodes():
     try:
         api_client = get_api_client()
         nodes = api_client.list_node()
+        return nodes
         worker_nodes = []
         for node in nodes.items:
             # Skip control plane
