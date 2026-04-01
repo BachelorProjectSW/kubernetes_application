24d2f4a22eade62f12d3775d09f47fb8081be106
DoubleO69420 <omid745433@gmail.com>
2026-04-01 11:25:15 +0200

    status

--- src/cluster_api/services/get_worker_nodes.py
+++ src/cluster_api/services/get_worker_nodes.py
@@ -18,7 +18,7 @@ def get_cluster_working_nodes():
                 continue
 
             name = node.metadata.name
-            status = node.conditions.status
+            status = node.condition.status
             worker_nodes.append({
                 "name": name,
                 "status": status,
