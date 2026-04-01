e27aae723a6b0ca7334a84a375637eec233c9d20
DoubleO69420 <omid745433@gmail.com>
2026-04-01 14:04:46 +0200

    ere

--- src/cluster_api/util/client_setup.py
+++ src/cluster_api/util/client_setup.py
@@ -4,7 +4,7 @@ from kubernetes import client, config
 def get_api_client():
     """Return a CoreV1Api client from the auth folder."""
     try:
-        config.load_incluster_config()
+        config.load_incluster_config() #NOT TESTED
     except config.ConfigException:
         config.load_kube_config()
     return client.CoreV1Api()
