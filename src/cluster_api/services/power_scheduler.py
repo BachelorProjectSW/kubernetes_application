ced7bece04600181761076349cc945da8f3c1125
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-04-01 15:33:42 +0200

    gg

--- src/cluster_api/services/power_scheduler.py
+++ src/cluster_api/services/power_scheduler.py
@@ -1,6 +1,10 @@
 from gpiozero import LED as IO
-
+import paramiko
+import structlog
 from .nodes import Cluster, WorkerNode, get_cluster
+
+log = structlog.get_logger()
+
 
 def turn_on_node(worker_node: WorkerNode):
     """Turn on the node."""
@@ -10,19 +14,26 @@ def turn_on_node(worker_node: WorkerNode):
     worker_node.status = "Turning on"
 
 
-def turn_on_nodes(number_of_nodes: int, cluster_name: str = "dk"):
-    """Turn on up to number_of_nodes inactive nodes in a cluster."""
+def change_node_status(number_of_nodes: int, cluster_name: str, status: str):
+    """Turn on up to number_of_nodes inactive nodes in a cluster.
+    status is either 'on' of 'off' """
     cluster = get_cluster(cluster_name)
-    nodes_to_turn_on = select_nodes_to_turn_on(number_of_nodes, cluster)
 
-    for node in nodes_to_turn_on:
-        turn_on_node(node)
-
+    if status == 'on':
+        nodes_to_change = select_nodes_to_turn_on(number_of_nodes, cluster)
+        for node in nodes_to_change:
+            turn_on_node(node)
+    elif status == 'off':
+        nodes_to_change = select_nodes_to_turn_off(number_of_nodes, cluster)
+        for node in nodes_to_change:
+            turn_off_node(node, cluster_name, cluster_name)
+    
     return {
         "cluster": cluster_name,
         "requested": number_of_nodes,
-        "turned_on": len(nodes_to_turn_on),
-        "nodes": [node.name for node in nodes_to_turn_on],
+        "status": status,
+        "node_changed": len(nodes_to_change),
+        "nodes": [node.name for node in nodes_to_change],
     }
 
 
@@ -35,3 +46,35 @@ def select_nodes_to_turn_on(number_of_nodes: int, cluster: Cluster) -> list[Work
         if node.status == "inactive":
             nodes_to_turn_on.append(node)
     return nodes_to_turn_on
+
+
+def select_nodes_to_turn_off(number_of_nodes: int, cluster: Cluster) -> list[WorkerNode]:
+    """This is just template code."""
+    nodes_to_turn_off = []
+    for node in cluster.nodes:
+        if len(nodes_to_turn_off) >= number_of_nodes:
+            break
+        if node.status == "active":
+            nodes_to_turn_off.append(node)
+    return nodes_to_turn_off
+
+
+def turn_off_node(worker_node: WorkerNode, username: str, password: str):
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        client.connect(hostname=worker_node.ip, username=username, password=password)
+
+        stdin, stdout, stderr = client.exec_command("sudo shutdown now")
+
+        stdin.write(password + "\n")
+        stdin.flush()
+
+        log.debug(stdout.read().decode())
+        log.debug(stderr.read().decode())
+
+        client.close()
+    except Exception as e:
+        log.debug("failed to shutdown node", e)
+
