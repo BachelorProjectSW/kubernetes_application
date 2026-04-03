0f5055630849dcffe207f3287be8a7b97834bce2
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-04-03 17:03:56 +0200

    gg

--- src/strato_api/services/start_test.py
+++ src/strato_api/services/start_test.py
@@ -3,8 +3,9 @@ from ...models.basemodels import *
 from test.k3d.cluster_configs.test_config import get_test_config
 def start_test(config: Config):
     """Start test to the global scheduler. Further this should run chron job ensure the next in queue begins"""
-    
-    url = "http://127.0.0.1:8020/start_test" #url should be to global scheduler
+    ip = config.global_scheduler.ip
+    port = config.global_scheduler.port
+    url = f"http://{ip}:{port}/start_test" #url should be to global scheduler
 
     response = requests.post(url, json=config.model_dump())
 
