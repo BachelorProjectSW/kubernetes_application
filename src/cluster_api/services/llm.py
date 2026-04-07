a170a2d2d8187b2dc689083b12d4766658f116f2
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-04-07 12:06:47 +0200

    gg

--- src/cluster_api/services/llm.py
+++ src/cluster_api/services/llm.py
@@ -11,7 +11,6 @@ def handle_llm(question: QuestionConfig):
         config = config_store.get()
         print(config.cluster_config.name)
         print(len(config.worker_nodes))
-        return "hey"
         worker_node = choose_worker_node(config.worker_nodes)
         # url = "http://llama-service:8080/completion"
         url = f"http://{worker_node.ip}:{config.cluster_config.llama_service_port}/completion"
