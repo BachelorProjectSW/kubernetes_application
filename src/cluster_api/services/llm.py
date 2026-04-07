ef907f177fe3b5d3dde9d2ff68b38ad45eac8708
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-04-07 12:14:04 +0200

    gg

--- src/cluster_api/services/llm.py
+++ src/cluster_api/services/llm.py
@@ -16,7 +16,6 @@ def handle_llm(question: QuestionConfig):
             "prompt": question.question,
             "n_predict": question.max_output_tokens,
             "temperature": 0
-
         }
 
         response = requests.post(
