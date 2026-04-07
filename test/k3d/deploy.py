f013897761fc0d8218b484bb0de6dd2c652ca7a3
DoubleO69420 <omid745433@gmail.com>
2026-04-07 10:14:32 +0200

    ci

--- test/k3d/deploy.py
+++ test/k3d/deploy.py
@@ -19,10 +19,23 @@ def deploy_clusters():
         run_cmd("kubectl wait --for=condition=Ready nodes --all --timeout=120s")
         for manifest in manifest_files:
             run_cmd(["kubectl", "apply", "-f", manifest])
-        run_cmd("kubectl wait --for=condition=Ready pod -l name=llama-server --timeout=180s")
-        run_cmd("kubectl get svc llama-service")
-        run_cmd("kubectl wait --for=jsonpath='{.subsets[0].addresses[0].ip}' "
-        "endpoints/llama-service --timeout=180s")
+        run_cmd([
+            "kubectl",
+            "wait",
+            "--for=condition=Ready",
+            "pod",
+            "-l",
+            "app=llama-server",
+            "--timeout=180s",
+        ])
+        run_cmd(["kubectl", "get", "svc", "llama-service"])
+        run_cmd([
+            "kubectl",
+            "wait",
+            "--for=jsonpath={.subsets[0].addresses[0].ip}",
+            "endpoints/llama-service",
+            "--timeout=180s",
+        ])
 
 
 if __name__ == "__main__":
