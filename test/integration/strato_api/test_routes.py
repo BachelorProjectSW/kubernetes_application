97bf7a21b9e84f6ed4047077fb1c9bf3571b37ee
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-04-07 16:11:51 +0200

    gg

---
+++ test/integration/strato_api/test_routes.py
@@ -0,0 +1,13 @@
+import pytest
+from ...k3d.cluster_configs.test_config import get_test_config
+import requests
+
+
+@pytest.mark.integration
+def test_add_test_to_queue_endpoint():
+    """Test."""
+    config = get_test_config()
+    url = f"http://{config.strato.ip}:{config.strato.port}/add_test_to_queue"
+    response = requests.post(url, json=config.model_dump())
+    assert len(response) == 1
+    assert response['test_config']['id'] == '123'
