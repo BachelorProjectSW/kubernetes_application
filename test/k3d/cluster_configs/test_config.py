c1e3bd7aa446bf33f357d1dd4668f55d0c3feaf2
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-04-07 13:37:33 +0200

    gg

--- test/k3d/cluster_configs/test_config.py
+++ test/k3d/cluster_configs/test_config.py
@@ -21,7 +21,7 @@ def get_test_config():
             max_ms=12000
         ),
         workload=WorkloadConfig(
-            request_per_minute=20,
+            request_per_minute=2,
             pattern="steady",
             seed=10,
             peakiness=0
