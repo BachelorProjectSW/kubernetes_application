8ed4346c100d017e4ca417ed4b7fd90de57d4b8a
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-04-07 12:24:18 +0200

    gg

--- src/cluster_api/services/llm.py
+++ src/cluster_api/services/llm.py
@@ -1,28 +1,38 @@
 import requests
 from ...models.basemodels import QuestionConfig, WorkerNode
 from ..util.cluster_config import config_store
-import httpx
 
 def choose_worker_node(worker_node_list: list[WorkerNode]) -> WorkerNode:
     worker_node = worker_node_list[0] #find a better solution
     return worker_node
 
-    
-
 def handle_llm(question: QuestionConfig):
-    config = config_store.get()
-
-    url = f"http://localhost:{config.cluster_config.llama_service_port}/completion"
-
-    payload = {
-        "prompt": question.question,
-        "n_predict": question.max_output_tokens,
-        "temperature": 0
-    }
-
     try:
-        with httpx.Client(timeout=60.0) as client:
-            response = client.post(url, json=payload)
-            return response.text
+        config = config_store.get()
+        worker_node = choose_worker_node(config.worker_nodes)
+        # url = "http://llama-service:8080/completion"
+        url = f"http://127.0.0.1:{config.cluster_config.llama_service_port}/models"
+        payload = {
+            "prompt": question.question,
+            "n_predict": question.max_output_tokens,
+            "temperature": 0
+        }
+
+        response = requests.post(
+            url,
+            json=payload,
+            timeout=60,
+        )
+
+        response.raise_for_status()
+
+        try:
+            return response.json()
+        except Exception:
+            # fallback: read raw text
+            text = response.text
+            print("RAW RESPONSE:", text)
+            return {"raw": text}
     except Exception as e:
         return f"failed: {e}"
+    
