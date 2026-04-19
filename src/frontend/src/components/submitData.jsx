ba8bfe394968210c2777452293a0ccac10160cb4
Zafir <Glund23@student.aau.dk>
2026-04-19 21:29:19 +0200

    commit 0.1

--- src/frontend/src/components/submitData.jsx
+++ src/frontend/src/components/submitData.jsx
@@ -10,132 +10,131 @@ export const handleSubmit = async (e, inputs) => {
 
 
     const exportData = {
-       /* id: inputs.expID || "",
-        name: inputs.name || "",
-        start: {
-            duration_time_s: totalSeconds || "",
-            start_time_simulated: inputs.startdate || "", // opdater andre steder
-            start_time_real: null
-        } || "",
 
-        weights: {
-            gco2: inputs.gco2,
-            cost: inputs.cost, 
-            latency: inputs.latency || "2"// opdater andre steder
-        } || "",
-        power_scheduler: {
-            start: true, 
-            timeout_s: inputs.timeout_s,
-            idle_time_for_turn_off_s: inputs.turn_off_s
-        } || "",
-        latency: {
-            latency_windows_s: inputs.window || "34", //opdater andre steder
-            max_latency: inputs.max_latency || "",
+        /* start: {
+             duration_time_s: totalSeconds || "",
+             start_time_simulated: inputs.startdate || "", // opdater andre steder
+             start_time_real: null
+         } || "",
+ 
+         weights: {
+             gco2: inputs.gco2,
+             cost: inputs.cost, 
+             latency: inputs.latency || "2"// opdater andre steder
+         } || "",
+         power_scheduler: {
+             start: true, 
+             timeout_s: inputs.timeout_s,
+             idle_time_for_turn_off_s: inputs.turn_off_s
+         } || "",
+         latency: {
+             latency_windows_s: inputs.window || "34", //opdater andre steder
+             max_latency: inputs.max_latency || "",
+         },
+         workload: {
+             request_pr_minute: inputs.request_pr_min, // opdate?
+             pattern: inputs.pattern,
+             seed: inputs.seed,
+             peakiness: inputs.peakiness
+         } || "",
+         question: {
+             question: inputs.question,
+             max_output_tokens: inputs.max_output_tokens,
+             context_window: inputs.context_window
+         } || "",
+         clusters: (inputs.clusters || []).map((cluster) => ({
+             name: cluster.name || "",
+             ip: cluster.ip || "",
+             port: cluster.port || "",
+             gpio_list: cluster.gpio_list ||"",
+             simulated_country_code: cluster.simulated_country_code || "",
+             llama_service_port: cluster.llama_service_port || "",
+             renewable_output_w: "200",
+             cluster_load_w: "1000",
+             grid_carbon_intensity: "100",
+             grid_electricity_price: "0.12",
+             k3d: false
+         })),
+         global_scheduler: {
+             ip: inputs.ip_global,
+             port: inputs.port_global
+         } || "",
+         strato: {
+             ip: inputs.ip_strato,
+             port: inputs.port_strato
+         } || ""
+          */
+        "id": stringify(inputs.expID) || "",
+        "name": stringify(inputs.name) || "",
+        "start": {
+            "duration_time_s": 30,
+            "start_time_simulated": "01/10/2021",
+            "start_time_real": null
         },
-        workload: {
-            request_pr_minute: inputs.request_pr_min, // opdate?
-            pattern: inputs.pattern,
-            seed: inputs.seed,
-            peakiness: inputs.peakiness
-        } || "",
-        question: {
-            question: inputs.question,
-            max_output_tokens: inputs.max_output_tokens,
-            context_window: inputs.context_window
-        } || "",
-        clusters: (inputs.clusters || []).map((cluster) => ({
-            name: cluster.name || "",
-            ip: cluster.ip || "",
-            port: cluster.port || "",
-            gpio_list: cluster.gpio_list ||"",
-            simulated_country_code: cluster.simulated_country_code || "",
-            llama_service_port: cluster.llama_service_port || "",
-            renewable_output_w: "200",
-            cluster_load_w: "1000",
-            grid_carbon_intensity: "100",
-            grid_electricity_price: "0.12",
-            k3d: false
-        })),
-        global_scheduler: {
-            ip: inputs.ip_global,
-            port: inputs.port_global
-        } || "",
-        strato: {
-            ip: inputs.ip_strato,
-            port: inputs.port_strato
-        } || ""
-         */
-  "id": "2",
-  "name": "RUUUUN!",
-  "start": {
-    "duration_time_s": 30,
-    "start_time_simulated": "01/10/2021",
-    "start_time_real": null
-  },
-  "weights": {
-    "gco2": 0.3,
-    "cost": 0.1,
-    "latency": 0.6
-  },
-  "power_scheduler": {
-    "start": true,
-    "timeout_s": 5,
-    "idle_time_for_turn_off_s": 1
-  },
-  "latency": {
-    "latency_window_s": 60,
-    "max_ms": 12000
-  },
-  "workload": {
-    "request_per_minute": 10,
-    "pattern": "steady",
-    "seed": 10,
-    "peakiness": 0
-  },
-  "question": {
-    "question": "hey",
-    "max_output_tokens": 200,
-    "context_window": 200
-  },
-  "clusters": [
-    {
-      "name": "dk",
-      "ip": "100.114.88.102",
-      "port": "8033",
-      "gpio_list": [17, 27, 23],
-      "simulated_country_code": "ES",
-      "llama_service_port": "8083",
-      "renewable_output_w": 200,
-      "cluster_load_w": 1000,
-      "grid_carbon_intensity": 100,
-      "grid_electricity_price": 0.12,
-      "k3d": false
-    },
-    {
-      "name": "pt",
-      "ip": "100.83.243.61",
-      "port": "8033",
-      "gpio_list": [17, 27, 23],
-      "simulated_country_code": "pt",
-      "llama_service_port": "8082",
-      "renewable_output_w": 400,
-      "cluster_load_w": 1000,
-      "grid_carbon_intensity": 300,
-      "grid_electricity_price": 0.14,
-      "k3d": false
-    }
-  ],
-  "global_scheduler": {
-    "ip": "100.84.252.101",
-    "port": "8022"
-  },
-  "strato": {
-    "ip": "100.109.95.2",
-    "port": "8011"
-  }
+        "weights": {
+            "gco2": 0.3,
+            "cost": 0.1,
+            "latency": 0.6
+        },
+        "power_scheduler": {
+            "start": true,
+            "timeout_s": 5,
+            "idle_time_for_turn_off_s": 1
+        },
+        "latency": {
+            "latency_window_s": 60,
+            "max_ms": 12000
+        },
+        "workload": {
+            "request_per_minute": 10,
+            "pattern": "steady",
+            "seed": 10,
+            "peakiness": 0
+        },
+        "question": {
+            "question": "hey",
+            "max_output_tokens": 200,
+            "context_window": 200
+        },
+        "clusters": [
+            {
+                "name": "dk",
+                "ip": "100.114.88.102",
+                "port": "8033",
+                "gpio_list": [17, 27, 23],
+                "simulated_country_code": "ES",
+                "llama_service_port": "8083",
+                "renewable_output_w": 200,
+                "cluster_load_w": 1000,
+                "grid_carbon_intensity": 100,
+                "grid_electricity_price": 0.12,
+                "k3d": false
+            },
+            {
+                "name": "pt",
+                "ip": "100.83.243.61",
+                "port": "8033",
+                "gpio_list": [17, 27, 23],
+                "simulated_country_code": "pt",
+                "llama_service_port": "8082",
+                "renewable_output_w": 400,
+                "cluster_load_w": 1000,
+                "grid_carbon_intensity": 300,
+                "grid_electricity_price": 0.14,
+                "k3d": false
+            }
+        ],
+        "global_scheduler": {
+            "ip": "100.84.252.101",
+            "port": "8022"
+        },
+        "strato": {
+            "ip": "100.109.95.2",
+            "port": "8011"
+        }
     };
 
-try {
+    try {
         console.log(exportData);
         const response = await fetch('http://100.109.95.2:8095/start_test', { // Ensure port matches your FastAPI server
             method: 'POST',
@@ -160,7 +159,7 @@ try {
         console.error("Network Error:", error);
         alert("Could not connect to the backend.");
     }
-     console.log(JSON.stringify(exportData, null, 2));
+    console.log(JSON.stringify(exportData, null, 2));
 };
 
 
