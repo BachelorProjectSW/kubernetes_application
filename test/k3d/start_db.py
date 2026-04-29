74e99a7329aafa41e1b5259d3296ced252b473b6
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-04-29 20:46:36 +0200

    8080 llama servers

--- test/k3d/start_db.py
+++ test/k3d/start_db.py
@@ -1,2 +1,3 @@
 from .utils import run_cmd
+run_cmd("docker rm -f p6-postgres")
 run_cmd("docker run --name p6-postgres -e POSTGRES_USER=strato -e POSTGRES_PASSWORD=strato -e POSTGRES_DB=strato -p 5433:5432 -v p6_pgdata:/var/lib/postgresql/data -d postgres:16")
