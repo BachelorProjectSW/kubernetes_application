6e3945e05259471635d493a4efb6abcfaeb9e09d
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2026-05-14 12:40:03 +0200

    cost tests

--- test/integration/test_k3d_integration.py
+++ test/integration/test_k3d_integration.py
@@ -278,7 +278,7 @@ def test_k3d_high_load():
 
 
 @pytest.mark.integration
-def test_k3d_switch_clusters():
+def test_k3d_switch_clusters_gco2():
     """Test it choose one cluster over another with weights and then change when data changed.
 
     In the beginning they both have very low PV (early morning).
@@ -293,6 +293,34 @@ def test_k3d_switch_clusters():
     config.workload.request_per_minute = 6
     config.weights.gco2 = 0.98
     config.weights.cost = 0.01
+    config.weights.latency = 0.01
+    config.clusters[0].simulated_country_code = "DE"  # DK control is simulated in Germany.
+    config.clusters[1].simulated_country_code = "PL"  # PT control is simulated in Poland.
+    (
+        K3dTestRunner(config)
+        .assert_total_requests(min_count=12, max_count=12)
+        .assert_success_rate(min_rate=1)
+        .assert_which_cluster_is_asserted(["dk","dk","dk","dk","dk","dk","pt","pt","pt","pt","pt","pt"], max_errors=0)
+        .run(config.start.duration_time_s)
+    )
+
+
+@pytest.mark.integration
+def test_k3d_switch_clusters_cost():
+    """Test it choose one cluster over another with weights and then change when data changed.
+
+    In the beginning they both have very low PV (early morning).
+    Therefore Germany it choosen due to lower gco2 (243 < 350.46)
+
+    But 08:00 pv is rising in poland and therefore renewable energy.
+    """
+    config = get_test_config()
+    config.name = config.name + "DE_AND_PL"
+    config.start.start_time_simulated = "26/02/2026 07:59:00"
+    config.start.duration_time_s = 120  # enough to overlap hours
+    config.workload.request_per_minute = 6
+    config.weights.gco2 = 0.01
+    config.weights.cost = 0.98
     config.weights.latency = 0.01
     config.clusters[0].simulated_country_code = "DE"  # DK control is simulated in Germany.
     config.clusters[1].simulated_country_code = "PL"  # PT control is simulated in Poland.
--- test/k3d/find_test_data.py
+++ test/k3d/find_test_data.py
@@ -225,6 +225,8 @@ class TestDataFinder:
             print(f"\nDAY {day + 1}: {current_date.date()}")
 
             for hour in range(24):
+                if hour >= 8 and hour <=16: 
+                    continue  
                 base_time = current_date + timedelta(hours=hour)
 
                 if self._check_timeline(base_time):
